bf998c28ec477cb0e3f69236592406c27ba43b42
Irena Shaigorodsky <ishaigorodsky@enservio.com>
2014-10-30 11:04:34 -0400

    Track uncaught exceptions for iOS

--- ios/GoogleAnalyticsPlugin.m
+++ ios/GoogleAnalyticsPlugin.m
@@ -115,4 +115,8 @@
   [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[GAI sharedInstance] setTrackUncaughtExceptions:YES;]
+}
+
 @end
