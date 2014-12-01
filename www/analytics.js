01499e39904f4dff08dd3504110f7223a5d2632d
Zoltan Magyar <zmagyar@gmail.com>
2014-12-01 08:44:13 +0100

    Removed cordova added lines

--- www/analytics.js
+++ www/analytics.js
@@ -1,4 +1,3 @@
-cordova.define("com.cmackay.plugins.googleanalytics.GoogleAnalytics", function(require, exports, module) { 
 /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -290,4 +289,3 @@ Analytics.prototype = {
 
 module.exports = new Analytics();
 
-});
