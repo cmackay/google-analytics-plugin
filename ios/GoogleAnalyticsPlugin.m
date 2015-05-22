f11f609152d6293863327f1d5974f74b8872a25d
Zoltan Magyar <zmagyar@gmail.com>
2015-05-22 11:03:51 +0200

    Fixed data layer problem for non string arguments

--- ios/GoogleAnalyticsPlugin.m
+++ ios/GoogleAnalyticsPlugin.m
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -43,93 +43,93 @@
  */
 - (void) setTrackingId: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* trackingId = [command.arguments objectAtIndex:0];
-
-  [GAI sharedInstance].dispatchInterval = 1;
-  [GAI sharedInstance].trackUncaughtExceptions = YES;
-
-  if (tracker) {
-    [[GAI sharedInstance] removeTrackerByName:[tracker name]];
-  }
-
-  tracker = [[GAI sharedInstance] trackerWithTrackingId:trackingId];
-  result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* trackingId = [command.arguments objectAtIndex:0];
+    
+    [GAI sharedInstance].dispatchInterval = 1;
+    [GAI sharedInstance].trackUncaughtExceptions = YES;
+    
+    if (tracker) {
+        [[GAI sharedInstance] removeTrackerByName:[tracker name]];
+    }
+    
+    tracker = [[GAI sharedInstance] trackerWithTrackingId:trackingId];
+    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) setLogLevel: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-
-  GAILogLevel logLevel = (GAILogLevel)[command.arguments objectAtIndex:0];
-
-  [[[GAI sharedInstance] logger] setLogLevel:logLevel];
-
-  result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    
+    GAILogLevel logLevel = (GAILogLevel)[command.arguments objectAtIndex:0];
+    
+    [[[GAI sharedInstance] logger] setLogLevel:logLevel];
+    
+    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) get: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-
-  if (!tracker) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
-  } else {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[tracker get:key]];
-  }
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    
+    if (!tracker) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
+    } else {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[tracker get:key]];
+    }
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) set: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-  NSString* value = [command.arguments objectAtIndex:1];
-
-  if (!tracker) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
-  } else {
-    [tracker set:key value:value];
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  }
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    NSString* value = [command.arguments objectAtIndex:1];
+    
+    if (!tracker) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
+    } else {
+        [tracker set:key value:value];
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) send: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSDictionary* params = [command.arguments objectAtIndex:0];
-
-  if (!tracker) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
-  } else {
-    [tracker send:params];
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  }
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSDictionary* params = [command.arguments objectAtIndex:0];
+    
+    if (!tracker) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
+    } else {
+        [tracker send:params];
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) close: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-
-  if (!tracker) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
-  } else {
-    [[GAI sharedInstance] removeTrackerByName:[tracker name]];
-    tracker = nil;
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  }
-
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    
+    if (!tracker) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"tracker not initialized"];
+    } else {
+        [[GAI sharedInstance] removeTrackerByName:[tracker name]];
+        tracker = nil;
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 
@@ -144,42 +144,42 @@
     NSString* containerId = [command.arguments objectAtIndex:0];
     self.tagManager = [TAGManager instance];
     self.containerOpenedCallbackId = command.callbackId;
-
+    
     // Optional: Change the LogLevel to Verbose to enable logging at VERBOSE and higher levels.
     [self.tagManager.logger setLogLevel:kTAGLoggerLogLevelVerbose];
-
+    
     /**
-     * we could open the container by the TAGContainerOpener however the callback handling 
-     * for that is not so sophisticated as the openContainerById:callback call so we rather 
+     * we could open the container by the TAGContainerOpener however the callback handling
+     * for that is not so sophisticated as the openContainerById:callback call so we rather
      * use that here
      *
-    [TAGContainerOpener openContainerWithId:containerId   // Update with your Container ID.
-    tagManager:self.tagManager
-    openType:kTAGOpenTypePreferNonDefault
-    timeout:&timeout
-    notifier:self];
-    */
+     [TAGContainerOpener openContainerWithId:containerId   // Update with your Container ID.
+     tagManager:self.tagManager
+     openType:kTAGOpenTypePreferNonDefault
+     timeout:&timeout
+     notifier:self];
+     */
     
     [self.tagManager openContainerById:containerId callback:self];
 }
 
 /**
-*
-* Refresh an already opened container
-*
-**/
+ *
+ * Refresh an already opened container
+ *
+ **/
 - (void) containerRefresh: (CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* result = nil;
     self.containerOpenedCallbackId = command.callbackId;
-
+    
     if (!self.container) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
         [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
     } else {
         [self.container refresh];
     }
-
+    
 }
 
 
@@ -240,72 +240,72 @@
 // >------
 
 /**
-*
-* Get GTM config value for the passed in key
-*
-*/
+ *
+ * Get GTM config value for the passed in key
+ *
+ */
 - (void) getContainerString: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-
-  if (!self.container) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
-  } else {
-    //
-    // Get the configuration value by key.
-    //
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[self.container stringForKey:key]];
-  }
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    
+    if (!self.container) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
+    } else {
+        //
+        // Get the configuration value by key.
+        //
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[self.container stringForKey:key]];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) getContainerBoolean: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-
-  if (!self.container) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
-  } else {
-    //
-    // Get the configuration value by key.
-    //
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:[self.container booleanForKey:key]];
-  }
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    
+    if (!self.container) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
+    } else {
+        //
+        // Get the configuration value by key.
+        //
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:[self.container booleanForKey:key]];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) getContainerLong: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-
-  if (!self.container) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
-  } else {
-    //
-    // Get the configuration value by key.
-    //
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:[self.container int64ForKey:key]];
-  }
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    
+    if (!self.container) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
+    } else {
+        //
+        // Get the configuration value by key.
+        //
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:[self.container int64ForKey:key]];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) getContainerDouble: (CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* result = nil;
-  NSString* key = [command.arguments objectAtIndex:0];
-
-  if (!self.container) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
-  } else {
-    //
-    // Get the configuration value by key.
-    //
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:[self.container doubleForKey:key]];
-  }
-  [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+    CDVPluginResult* result = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+    
+    if (!self.container) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"container not opened"];
+    } else {
+        //
+        // Get the configuration value by key.
+        //
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:[self.container doubleForKey:key]];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 }
 
 - (void) dataLayerValue: (CDVInvokedUrlCommand*)command
@@ -324,9 +324,7 @@
             //
             // Value can be sting/dictionary
             //
-            if ([value isKindOfClass:[NSString class]]) {
-                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%@", value]];
-            } else {
+            if ([value isKindOfClass:[NSDictionary class]]) {
                 NSError *error;
                 NSData *jsonData = [NSJSONSerialization dataWithJSONObject:value
                                                                    options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
@@ -338,14 +336,17 @@
                     NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
                     result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
                 }
+                
+            } else {
+                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%@", value]];
             }
-
+            
         }
-    
+        
     }
     
     [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
-    }
+}
 
 - (void) dataLayerPushValue: (CDVInvokedUrlCommand*)command
 {
@@ -385,7 +386,7 @@
         }
     }
     [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
-
+    
 }
 
 @end
